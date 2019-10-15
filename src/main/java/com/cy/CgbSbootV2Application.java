f9d146301f6c02299124677ff6b157d7da3ca91b
haungjia-cgb <Tedu@Tedu-PC>
2019-10-15 09:05:04 +0800

    提交了第一个工程

---
+++ src/main/java/com/cy/CgbSbootV2Application.java
@@ -0,0 +1,13 @@
+package com.cy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgbSbootV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgbSbootV2Application.class, args);
+	}
+
+}
