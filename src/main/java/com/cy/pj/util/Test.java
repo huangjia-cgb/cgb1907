2e97eeca12d44a717930b12384d1ce5d90721177
haungjia-cgb <56516722+huangjia-cgb@users.noreply.github.com>
2020-05-20 17:21:34 +0800

    写了一个测试代码

---
+++ src/main/java/com/cy/pj/util/Test.java
@@ -0,0 +1,39 @@
+package com.cy.pj.util;
+
+import java.util.ArrayList;
+import java.util.List;
+class Student{
+     public  String name;
+     public  String course;
+     public int score;
+     public  int total;
+    public Student(){};
+    public Student(String name, String course, int score) {
+        this.name = name;
+        this.course = course;
+        this.score = score;
+    }
+}
+public class Test {
+    public static void main(String[] args) {
+        List <Student>list = new ArrayList<>();
+        list.add(new Student("张三","语文",60));
+        list.add(new Student("张三","数学",70));
+        list.add(new Student("李四","语文",65));
+        list.add(new Student("李四","语文",75));
+
+        Student result = null ;
+        String name =null;
+        int total =0;
+        for (Student stu :list){
+            if (!stu.name.equals(name)){
+                result = new Student();
+                total=0;
+            }
+            total =total+stu.score;
+            result.total=total;
+            name=stu.name;
+        }
+        System.out.println(result.total);
+    }
+}
